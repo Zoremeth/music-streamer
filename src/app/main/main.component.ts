e7e3c6d74ebaf03a7901b88ea1f62089c1a7a0c9
Zoremeth <zoremeth@gmail.com>
2018-11-10 23:30:50 +0100

    Feat: Song list, Improv: Different font

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -8,15 +8,20 @@ import { MusicControlService } from '../shared/music-control.service';
 })
 export class MainComponent implements OnInit {
 
+  songs = [
+      { "url": "http://localhost/&Z.flac", "title": "&Z", "artist": "Mizayuki", "album": "Aldnoah Zero"},
+      { "url": "http://localhost/AboutMe.ogg", "title": "About Me", "artist": "Nicode", "album": "About Me - Nicode"},
+  ]
+
   constructor(private musicService: MusicControlService) { }
 
   ngOnInit() {
   }
 
   play(streamURL: string): void {
-    const prompt = <string>window.prompt("Enter a URL", "http://domain.tld/");
+    // const prompt = <string>window.prompt("Enter a URL", "http://domain.tld/");
     const musicPlayer = <HTMLAudioElement>document.getElementById('musicplayer');
-    this.musicService.play(prompt);
+    this.musicService.play(streamURL);
     // this.musicService.seekbarMax = Math.trunc(musicPlayer.duration);
   }
 }
